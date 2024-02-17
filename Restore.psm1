31f2d6845f3eec9b8ca16908062a734094e56b30
Ulrik Brun <64921639+spkier12@users.noreply.github.com>
2024-02-17 22:09:14 +0100

    Add files via upload

---
+++ Restore.psm1
@@ -0,0 +1,46 @@
+﻿$WorkingDirectory = pwd
+Import-Module -Name "$WorkingDirectory\Settings.psm1"
+
+$GetBackupPath = New-Object System.Collections.ArrayList
+
+# Restores data from the backup folders and place the .zip folders back into their original locations
+function RestoreDataRP {
+    $FolderCopying = ""
+    try {
+        foreach ($item in $GetBackupPath) {
+            $GetBackupPath2 = $item.Split("\")
+            $NameFolder2 = $GetBackupPath2[$GetBackupPath2.Count -1]
+            $FolderCopying = $item
+
+            # Remove the folder name to get the true path
+            $PathOfRestore = $item.Replace("$NameFolder2", "")
+
+            Write-Host "Copying Folder: $GDrive$NameFolder2 to $PathOfRestore$NameFolder2"
+
+            # Extract the .zip file into the restore path
+            Expand-Archive -Path "$GDrive$NameFolder2.zip" -Destination "$PathOfRestore" -Force
+
+            # Remove the path from array so it wont re-try agen
+            $GetBackupPath.Remove($item)
+
+        }
+    } catch {
+        $GetBackupPath.Remove($FolderCopying)
+        Write-Host "`n Error: $FolderCopying [Unkown File]" -BackgroundColor Red
+        RestoreDataRP
+    }
+}
+
+function GetRestoreRP {
+    $GetContent = Get-Content $BackupList
+    $SplitContent = $GetContent.Split("`n")
+
+    foreach ($item in $SplitContent) {
+        $GetBackupPath.Add("$Ufolders$item")
+    }
+
+    RestoreDataRP
+}
+
+Write-Host "`n Loaded Restore.psm1"
+Export-ModuleMember -Function * -Alias *
---
+++ Settings.psm1
@@ -0,0 +1,10 @@
+﻿$WorkingDirectory = pwd
+$BackupList = "$WorkingDirectory\Bfolders.txt"
+$GDrive = "G:\Min disk\Backup\"
+
+$Username = [System.Environment]::UserName
+$Ufolders = "C:\Users\$Username"
+
+Write-Host "`n Loaded Settings.psm1"
+
+Export-ModuleMember -Variable * -Alias *
